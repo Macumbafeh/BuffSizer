7e3ef4547aeed8fc869236ec59025b1a6422337a
Schaka <schakaqt@hotmail.com>
2014-06-08 18:37:36 +0200

    add focus functionality

--- BuffBelow.lua
+++ BuffBelow.lua
@@ -1,20 +1,23 @@
---CURRENT_TARGET_NUM_DEBUFFS
---TARGET_BUFFS_PER_ROW
-function BuffSizer_Below()
-	local targetBuffSize = BuffSizerDB.buffSize
-	local targetDebuffSize = BuffSizerDB.debuffSize
-	local buffOffset = BuffSizerDB.buffOffset
-	local debuffOffset = BuffSizerDB.debuffOffset
 
+local targetBuffSize = BuffSizerDB.targetbuffSize
+local targetDebuffSize = BuffSizerDB.targetdebuffSize
+local focusBuffSize = BuffSizerDB.focusbuffSize
+local focusDebuffSize = BuffSizerDB.focusdebuffSize
+
+local buffOffset = BuffSizerDB.buffOffset
+local debuffOffset = BuffSizerDB.debuffOffset
+	
+	
+function BuffSizer_TargetBelow()
 
 	-- Update buff positioning/size
 	function TargetFrame_UpdateBuffAnchor(buffName, index, numFirstRowBuffs, numDebuffs, buffSize, offset, ...)
 		local buff = getglobal(buffName..index);
-		buffSize = BuffSizerDB.buffSize
+		buffSize = BuffSizerDB.targetbuffSize
 		offset = buffOffset
 		-- buff/debuff area is ~55% of the frame, portait comes after that
 		local i = 1
-		while (TargetFrame:GetWidth()*0.55) > (BuffSizerDB.buffSize*i) do
+		while (TargetFrame:GetWidth()*0.55) > (BuffSizerDB.targetbuffSize*i) do
 			numFirstRowBuffs = i
 			i = i + 1	
 		end
@@ -58,11 +61,11 @@ function BuffSizer_Below()
 	-- Update debuff positioning/size
 	function TargetFrame_UpdateDebuffAnchor(buffName, index, numFirstRowBuffs, numBuffs, buffSize, offset, ...)
 		local buff = getglobal(buffName..index);
-		buffSize = BuffSizerDB.debuffSize
+		buffSize = BuffSizerDB.targetdebuffSize
 		offset = debuffOffset
 		-- buff/debuff area is ~55% of the frame, portait comes after that
 		local i = 1
-		while (TargetFrame:GetWidth()*0.55) > (BuffSizerDB.debuffSize*i) do
+		while (TargetFrame:GetWidth()*0.55) > (BuffSizerDB.targetdebuffSize*i) do
 			numFirstRowBuffs = i
 			i = i + 1	
 		end
@@ -105,10 +108,10 @@ function BuffSizer_Below()
 	function Target_Spellbar_AdjustPosition()
 		local yPos = 5;
 		local rowSize = 0
-		if BuffSizerDB.buffSize < BuffSizerDB.debuffSize then
-			rowSize = BuffSizerDB.debuffSize
+		if BuffSizerDB.targetbuffSize < BuffSizerDB.targetdebuffSize then
+			rowSize = BuffSizerDB.targetdebuffSize
 		else
-			rowSize = BuffSizerDB.buffSize
+			rowSize = BuffSizerDB.targetbuffSize
 		end
 		if ( TargetFrame.buffRows and TargetFrame.buffRows <= 2 ) then
 			yPos = 15 + rowSize;
@@ -127,5 +130,128 @@ function BuffSizer_Below()
 			end
 		end
 		TargetFrameSpellBar:SetPoint("BOTTOM", "TargetFrame", "BOTTOM", -15, -yPos);
+	end
+	
+		function FocusFrame_UpdateBuffAnchor(buffName, index, numFirstRowBuffs, numDebuffs, buffSize, offset, ...)
+		local buff = getglobal(buffName..index);
+		buffSize = BuffSizerDB.focusbuffSize
+		offset = buffOffset
+		-- buff/debuff area is ~55% of the frame, portait comes after that
+		local i = 1
+		while (FocusFrame:GetWidth()*0.55) > (BuffSizerDB.focusbuffSize*i) do
+			numFirstRowBuffs = i
+			i = i + 1	
+		end
+		
+		if ( index == 1 ) then
+			if ( UnitIsFriend("player", "Focus") ) then
+				buff:SetPoint("TOPLEFT", FocusFrame, "BOTTOMLEFT", FocusFrame.buffStartX, FocusFrame.buffStartY);
+			else
+				if ( numDebuffs > 0 ) then
+					buff:SetPoint("TOPLEFT", FocusFrameDebuffs, "BOTTOMLEFT", 0, -FocusFrame.buffSpacing);
+				else
+					buff:SetPoint("TOPLEFT", FocusFrame, "BOTTOMLEFT", FocusFrame.buffStartX, FocusFrame.buffStartY);
+				end
+			end
+			FocusFrameBuffs:SetPoint("TOPLEFT", buff, "TOPLEFT", 0, 0);
+			FocusFrameBuffs:SetPoint("BOTTOMLEFT", buff, "BOTTOMLEFT", 0, 0);
+			FocusFrame.buffRows = FocusFrame.buffRows+1;
+		elseif ( index == (numFirstRowBuffs+1) ) then
+			buff:SetPoint("TOPLEFT", getglobal(buffName..1), "BOTTOMLEFT", 0, -FocusFrame.buffSpacing);
+			FocusFrameBuffs:SetPoint("BOTTOMLEFT", buff, "BOTTOMLEFT", 0, 0);
+			FocusFrame.buffRows = FocusFrame.buffRows+1;
+		elseif ( hasTargetofFocus and index == (2*numFirstRowBuffs+1) ) then
+			buff:SetPoint("TOPLEFT", getglobal(buffName..(numFirstRowBuffs+1)), "BOTTOMLEFT", 0, -FocusFrame.buffSpacing);
+			FocusFrameBuffs:SetPoint("BOTTOMLEFT", buff, "BOTTOMLEFT", 0, 0);
+			FocusFrame.buffRows = FocusFrame.buffRows+1;
+		elseif ( (index > numFirstRowBuffs) and (mod(index+(Focus_BUFFS_PER_ROW-numFirstRowBuffs), Focus_BUFFS_PER_ROW) == 1) and not hasTargetofFocus ) then
+			-- Make a new row, have to take the number of buffs in the first row into account
+			buff:SetPoint("TOPLEFT", getglobal(buffName..(index-Focus_BUFFS_PER_ROW)), "BOTTOMLEFT", 0, -FocusFrame.buffSpacing);
+			FocusFrameBuffs:SetPoint("BOTTOMLEFT", buff, "BOTTOMLEFT", 0, 0);
+			FocusFrame.buffRows = FocusFrame.buffRows+1;
+		else
+			-- Just anchor to previous
+			buff:SetPoint("TOPLEFT", getglobal(buffName..(index-1)), "TOPRIGHT", offset, 0);
+		end
+
+		-- Resize
+		buff:SetWidth(buffSize);
+		buff:SetHeight(buffSize);
+	end
+end
+
+function BuffSizer_FocusBelow()
+	-- Update debuff positioning/size
+	function FocusFrame_UpdateDebuffAnchor(buffName, index, numFirstRowBuffs, numBuffs, buffSize, offset, ...)
+		local buff = getglobal(buffName..index);
+		buffSize = BuffSizerDB.focusdebuffSize
+		offset = debuffOffset
+		-- buff/debuff area is ~55% of the frame, portait comes after that
+		local i = 1
+		while (FocusFrame:GetWidth()*0.55) > (BuffSizerDB.focusdebuffSize*i) do
+			numFirstRowBuffs = i
+			i = i + 1	
+		end
+		
+		if ( index == 1 ) then
+			if ( UnitIsFriend("player", "Focus") and (numBuffs > 0) ) then
+				buff:SetPoint("TOPLEFT", FocusFrameBuffs, "BOTTOMLEFT", 0, -FocusFrame.buffSpacing);
+			else
+				buff:SetPoint("TOPLEFT", FocusFrame, "BOTTOMLEFT", FocusFrame.buffStartX, FocusFrame.buffStartY);
+			end
+			FocusFrameDebuffs:SetPoint("TOPLEFT", buff, "TOPLEFT", 0, 0);
+			FocusFrameDebuffs:SetPoint("BOTTOMLEFT", buff, "BOTTOMLEFT", 0, 0);
+			FocusFrame.buffRows = FocusFrame.buffRows+1;
+		elseif ( index == (numFirstRowBuffs+1) ) then
+			buff:SetPoint("TOPLEFT", getglobal(buffName..1), "BOTTOMLEFT", 0, -FocusFrame.buffSpacing);
+			FocusFrameDebuffs:SetPoint("BOTTOMLEFT", buff, "BOTTOMLEFT", 0, 0);
+			FocusFrame.buffRows = FocusFrame.buffRows+1;
+		elseif ( hasTargetofFocus and index == (2*numFirstRowBuffs+1) ) then
+			buff:SetPoint("TOPLEFT", getglobal(buffName..(numFirstRowBuffs+1)), "BOTTOMLEFT", 0, -FocusFrame.buffSpacing);
+			FocusFrameDebuffs:SetPoint("BOTTOMLEFT", buff, "BOTTOMLEFT", 0, 0);
+			FocusFrame.buffRows = FocusFrame.buffRows+1;
+		elseif ( (index > numFirstRowBuffs) and (mod(index+(Focus_DEBUFFS_PER_ROW-numFirstRowBuffs), Focus_DEBUFFS_PER_ROW) == 1) and not hasTargetofFocus ) then
+			-- Make a new row
+			buff:SetPoint("TOPLEFT", getglobal(buffName..(index-Focus_DEBUFFS_PER_ROW)), "BOTTOMLEFT", 0, -FocusFrame.buffSpacing);
+			FocusFrameDebuffs:SetPoint("BOTTOMLEFT", buff, "BOTTOMLEFT", 0, 0);
+			FocusFrame.buffRows = FocusFrame.buffRows+1;
+		else
+			-- Just anchor to previous
+			buff:SetPoint("TOPLEFT", getglobal(buffName..(index-1)), "TOPRIGHT", offset, 0);
+		end
+		
+		-- Resize
+		buff:SetWidth(buffSize);
+		buff:SetHeight(buffSize);
+		local debuffFrame = getglobal(buffName..index.."Border");
+		debuffFrame:SetWidth(buffSize+2);
+		debuffFrame:SetHeight(buffSize+2);
+	end
+
+	function Focus_Spellbar_AdjustPosition()
+		local yPos = 5;
+		local rowSize = 0
+		if BuffSizerDB.focusbuffSize < BuffSizerDB.focusdebuffSize then
+			rowSize = BuffSizerDB.focusdebuffSize
+		else
+			rowSize = BuffSizerDB.focusbuffSize
+		end
+		if ( FocusFrame.buffRows and FocusFrame.buffRows <= 2 ) then
+			yPos = 15 + rowSize;
+		elseif ( FocusFrame.buffRows ) then
+			yPos = 15 + rowSize* FocusFrame.buffRows
+		end
+		if ( TargetofFocusFrame:IsShown() ) then
+			if ( yPos <= 25 ) then
+				yPos = yPos + 25;
+			end
+		else
+			yPos = yPos - 5;
+			local classification = UnitClassification("Focus");
+			if ( (yPos < rowSize) and ((classification == "worldboss") or (classification == "rareelite") or (classification == "elite") or (classification == "rare")) ) then
+				yPos = rowSize;
+			end
+		end
+		FocusFrameSpellBar:SetPoint("BOTTOM", "FocusFrame", "BOTTOM", -15, -yPos);
 	end
 end
--- BuffSizer.lua
+++ BuffSizer.lua
@@ -1,6 +1,6 @@
 local function log(msg) DEFAULT_CHAT_FRAME:AddMessage(msg) end -- alias for convenience
 
-BuffSizerDB = BuffSizerDB or { buffSize = 25, debuffSize = 25, buffOffset = 2, debuffOffset = 2, type = "below" }
+BuffSizerDB = BuffSizerDB or { targetbuffSize = 25, targetdebuffSize = 25, focusbuffSize = 25, focusdebuffSize = 25, buffOffset = 2, debuffOffset = 2, targetType = "below", focusType = "below" }
 local SO = LibStub("LibSimpleOptions-1.0")
 
 local BuffSizer = CreateFrame("Frame", "BuffSizer", UIParent)
@@ -20,12 +20,20 @@ function BuffSizer:PLAYER_LOGIN(...)
 end
 
 function BuffSizer:CallBuffType()
-	if BuffSizerDB.type == "below" then
-		BuffSizer_Below()
-	elseif BuffSizerDB.type == "TopBuffToDebuff" then
-		BuffSizer_TopBuffToDebuff()
-	elseif BuffSizerDB.type == "TopDebuffToBuff" then
-		BuffSizer_TopDebuffToBuff()
+	if BuffSizerDB.targetType == "below" then
+		BuffSizer_TargetBelow()
+	elseif BuffSizerDB.targetType == "TopBuffToDebuff" then
+		BuffSizer_TargetTopBuffToDebuff()
+	elseif BuffSizerDB.targetType == "TopDebuffToBuff" then
+		BuffSizer_TargetTopDebuffToBuff()
+	end
+	
+	if BuffSizerDB.focusType == "below" then
+		BuffSizer_FocusBelow()
+	elseif BuffSizerDB.focusType == "TopBuffToDebuff" then
+		BuffSizer_FocusTopBuffToDebuff()
+	elseif BuffSizerDB.focusType == "TopDebuffToBuff" then
+		BuffSizer_FocusTopDebuffToBuff()
 	end
 end
 
@@ -36,8 +44,8 @@ function BuffSizer:CreateOptions()
 	SO.AddSlashCommand("BuffSizer","/bsizer")
 	SO.AddSlashCommand("BuffSizer","/bs")
 	local title, subText = panel:MakeTitleTextAndSubText("BuffSizer", "General settings")
-	local buffScale = panel:MakeSlider(
-	     'name', 'Buff size',
+	local targetbuffScale = panel:MakeSlider(
+	     'name', 'Target buff size',
 	     'description', 'Set the size of buff icons',
 	     'minText', '10',
 	     'maxText', '50',
@@ -45,13 +53,13 @@ function BuffSizer:CreateOptions()
 	     'maxValue', 50,
 	     'step', 1,
 	     'default', 15,
-	     'current', BuffSizerDB.buffSize,
-	     'setFunc', function(value) BuffSizerDB.buffSize = value end,
+	     'current', BuffSizerDB.targetbuffSize,
+	     'setFunc', function(value) BuffSizerDB.targetbuffSize = value end,
 	     'currentTextFunc', function(value) return string.format("%.2f",value) end)
-	buffScale:SetPoint("TOPLEFT",subText,"TOPLEFT",16,-32)
+	targetbuffScale:SetPoint("TOPLEFT",subText,"TOPLEFT",16,-32)
 	
-	local debuffScale = panel:MakeSlider(
-	     'name', 'Debuff size',
+	local targetdebuffScale = panel:MakeSlider(
+	     'name', 'Target debuff size',
 	     'description', 'Set the size of debuff icons',
 	     'minText', '10',
 	     'maxText', '50',
@@ -59,13 +67,13 @@ function BuffSizer:CreateOptions()
 	     'maxValue', 50,
 	     'step', 1,
 	     'default', 15,
-	     'current', BuffSizerDB.debuffSize,
-	     'setFunc', function(value) BuffSizerDB.debuffSize = value end,
+	     'current', BuffSizerDB.targetdebuffSize,
+	     'setFunc', function(value) BuffSizerDB.targetdebuffSize = value end,
 	     'currentTextFunc', function(value) return string.format("%.2f",value) end)
-	debuffScale:SetPoint("TOPLEFT",buffScale,"TOPRIGHT",16,0)
+	targetdebuffScale:SetPoint("TOPLEFT",targetbuffScale,"TOPRIGHT",16,0)
 	
-	local buffType = panel:MakeDropDown(
-	    'name', 'Buff display type',
+	local targetbuffType = panel:MakeDropDown(
+	    'name', 'Target buff display type',
 	    'description', 'Choose which way buffs are shown',
 	    'values', {
 	        'below', "Below",
@@ -73,7 +81,48 @@ function BuffSizer:CreateOptions()
 	        'TopDebuffToBuff', "On top (debuffs first)",
 	     },
 	    'default', 'below',
-	    'current', BuffSizerDB.type,
-	    'setFunc', function(value) BuffSizerDB.type = value BuffSizer:CallBuffType() end)
-	buffType:SetPoint("TOPLEFT", buffScale, "BOTTOMLEFT", -15, -35)	
+	    'current', BuffSizerDB.targetType,
+	    'setFunc', function(value) BuffSizerDB.targetType = value BuffSizer:CallBuffType() end)
+	targetbuffType:SetPoint("TOPLEFT", targetbuffScale, "BOTTOMLEFT", -15, -35)
+
+	local focusbuffScale = panel:MakeSlider(
+	     'name', 'Focus buff size',
+	     'description', 'Set the size of buff icons',
+	     'minText', '10',
+	     'maxText', '50',
+	     'minValue', 10,
+	     'maxValue', 50,
+	     'step', 1,
+	     'default', 15,
+	     'current', BuffSizerDB.focusbuffSize,
+	     'setFunc', function(value) BuffSizerDB.focusbuffSize = value end,
+	     'currentTextFunc', function(value) return string.format("%.2f",value) end)
+	focusbuffScale:SetPoint("TOPLEFT",targetbuffType,"TOPLEFT",16,-40)
+	
+	local focusdebuffScale = panel:MakeSlider(
+	     'name', 'Focus debuff size',
+	     'description', 'Set the size of debuff icons',
+	     'minText', '10',
+	     'maxText', '50',
+	     'minValue', 10,
+	     'maxValue', 50,
+	     'step', 1,
+	     'default', 15,
+	     'current', BuffSizerDB.focusdebuffSize,
+	     'setFunc', function(value) BuffSizerDB.focusdebuffSize = value end,
+	     'currentTextFunc', function(value) return string.format("%.2f",value) end)
+	focusdebuffScale:SetPoint("TOPLEFT",focusbuffScale,"TOPRIGHT",16,0)
+	
+	local focusbuffType = panel:MakeDropDown(
+	    'name', 'Focus buff display type',
+	    'description', 'Choose which way buffs are shown',
+	    'values', {
+	        'below', "Below",
+	        'TopBuffToDebuff', "On top (buffs first)",
+	        'TopDebuffToBuff', "On top (debuffs first)",
+	     },
+	    'default', 'below',
+	    'current', BuffSizerDB.focusType,
+	    'setFunc', function(value) BuffSizerDB.focusType = value BuffSizer:CallBuffType() end)
+	focusbuffType:SetPoint("TOPLEFT", focusbuffScale, "BOTTOMLEFT", -15, -35)		
 end
